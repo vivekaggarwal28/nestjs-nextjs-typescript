2c1e131cdfd5287301460868d58c1efc08a1977f
Vivek Aggarwal <vivek281187@gmail.com>
2022-07-17 13:04:41 +1000

    add events controller

---
+++ todo.js
@@ -0,0 +1,19 @@
+/* NodeJS Libraries to Consider: https://leanylabs.com/blog/npm-packages-for-nodejs/
+1) Fastify
+2) Socket.IO
+3) Validator: String validator
+4) Yup: Schema builder
+5) Day.js
+6) jsonwebtoken
+7) Bcrypt
+8) UUID
+9) fs-extra
+10) glob
+11) jest
+12) winston: logger
+13) Debug
+14) Passport: authentication middleware
+15) Nodemailer 
+16) Django 
+17) Swagger
+*/
