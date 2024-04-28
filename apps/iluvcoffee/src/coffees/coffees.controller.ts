d11205f992ff02b36c2b89b84d5ed5441f450766
Vivek Aggarwal <vivek281187@gmail.com>
2024-04-28 18:15:53 +1000

    add coffee controller

---
+++ apps/iluvcoffee/src/coffees/coffees.controller.ts
@@ -0,0 +1,44 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+  Res,
+} from '@nestjs/common';
+
+@Controller('coffees')
+export class CoffeesController {
+  @Get()
+  findAll(@Res() response, @Query() paginationQuery) {
+    const { limit, offset } = paginationQuery;
+    response
+      .status(200)
+      .send(
+        `This action returns all the coffees. Limit: ${limit}, Offset: ${offset}`,
+      );
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action returns #${id} coffee`;
+  }
+
+  @Post()
+  create(@Body() body: any) {
+    return body;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body: any) {
+    return `This action updates #${id} coffee`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes #${id} coffee`;
+  }
+}
--- apps/iluvcoffee/src/iluvcoffee.module.ts
+++ apps/iluvcoffee/src/iluvcoffee.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { IluvcoffeeController } from './iluvcoffee.controller';
 import { IluvcoffeeService } from './iluvcoffee.service';
+import { CoffeesController } from './coffees/coffees.controller';
 
 @Module({
   imports: [],
-  controllers: [IluvcoffeeController],
+  controllers: [IluvcoffeeController, CoffeesController],
   providers: [IluvcoffeeService],
 })
 export class IluvcoffeeModule {}
